164c760edca6dcd234d2a05ac45a2936aebb004b
Fraz Bakht <fbakht@leeddev.io>
2021-07-07 14:11:33 +0500

    Added libs folder.

---
+++ libs/apiGateway.ts
@@ -0,0 +1,33 @@
+import { AnySchemaObject } from 'ajv';
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import type { FromSchema } from 'json-schema-to-ts';
+
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> };
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>;
+
+export const formatJSONResponse = (response: Record<string, unknown>) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+    },
+    body: JSON.stringify(response),
+  };
+};
+
+export const successResponse: any = (statusCode = 200, message = '', data: AnySchemaObject = {}) => {
+  return {
+    statusCode: statusCode,
+    message: message,
+    data: data,
+  };
+};
+
+export const errorResponse: any = (statusCode = 400, message = 'error') => {
+  return {
+    statusCode: statusCode,
+    message: message,
+  };
+};
